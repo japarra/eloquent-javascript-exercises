97035eb92258c2c37b5ad77b144bd7c09ff2febb
Jordan Parra <japarra4@asu.com>
2015-09-14 04:38:59 +0000

    add solution to exercise 2

---
+++ chapter_02/exercise_2.js
@@ -0,0 +1,28 @@
+/**
+ * Write a program that uses console.log to print all the numbers from 1 to 100,
+ * with two exceptions. For numbers divisible by 3, print "Fizz" instead of the 
+ * number, and for numbers divisible by 5 (and not 3), print "Buzz" instead.
+ *
+ * When you have that working, modify your program to print "FizzBuzz", for 
+ * numbers that are divisible by both 3 and 5 (and still print "Fizz" or "Buzz" 
+ * for numbers divisible by only one of those).
+ */
+ 
+for (var num = 1; num <= 100; num++) {
+  if ((num % 3 === 0) && (num % 5 === 0)) {
+    console.log("FizzBuzz");
+    continue;
+  }
+
+  if (num % 3 === 0) {
+    console.log("Fizz");
+    continue;
+  }
+  
+  if (num % 5 === 0) {
+    console.log("Buzz");
+    continue;
+  }
+
+  console.log(num);
+}
